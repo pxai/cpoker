668a49d26b28e714ed08bd74bb9a92b40ea923db
pello.altadill <pello.altadill@4352fdf8-0407-11df-b14f-6fdb7bcc7550>
2010-04-28 09:01:32 +0000

    ai skeleton

---
+++ trunk/ai.h
@@ -0,0 +1,18 @@
+/**
+* $Id$
+*/
+#ifndef AI_H
+#define AI_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "deck.h"
+#include "players.h"
+#include "dealer.h"
+
+
+
+int decide();
+
+
+#endif
